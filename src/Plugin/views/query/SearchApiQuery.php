9b89c326530a4482c390251df5a38f546686a3e9
Thomas Seidl <remus@gmx.net>
2019-05-28 19:11:23 +0200

    Issue #2840272 by drunken monkey: Fixed fatal error in certain Views error conditions.

--- src/Plugin/views/query/SearchApiQuery.php
+++ src/Plugin/views/query/SearchApiQuery.php
@@ -16,6 +16,7 @@ use Drupal\search_api\ParseMode\ParseModeInterface;
 use Drupal\search_api\Plugin\views\field\SearchApiStandard;
 use Drupal\search_api\Plugin\views\ResultRow;
 use Drupal\search_api\Processor\ConfigurablePropertyInterface;
+use Drupal\search_api\Query\ConditionGroup;
 use Drupal\search_api\Query\ConditionGroupInterface;
 use Drupal\search_api\Query\QueryInterface;
 use Drupal\search_api\Query\ResultSetInterface;
@@ -861,7 +862,7 @@ class SearchApiQuery extends QueryPluginBase {
     if (!$this->shouldAbort()) {
       return $this->query->createConditionGroup($conjunction, $tags);
     }
-    return NULL;
+    return new ConditionGroup($conjunction, $tags);
   }
 
   /**
