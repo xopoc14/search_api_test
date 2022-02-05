4449ee4218a705eec1c535814623257c43607fc0
Thomas Seidl <remus@gmx.net>
2022-02-05 22:08:40 +0100

    Issue #3262702 by drunken monkey: Fixed test failures on Drupal 9.2.

--- tests/src/Kernel/Views/ViewsQueryTypeUpdateTest.php
+++ tests/src/Kernel/Views/ViewsQueryTypeUpdateTest.php
@@ -55,8 +55,7 @@ class ViewsQueryTypeUpdateTest extends KernelTestBase {
    */
   public function testViewUpdate() {
     // Create the view with faulty properties.
-    $module_path = $this->container->get('extension.path.resolver')
-      ->getPath('module', 'search_api');
+    $module_path = drupal_get_path('module', 'search_api');
     $view_yml = file_get_contents("$module_path/tests/fixtures/views.view.search_api_query_type_test.yml");
     $values = Yaml::decode($view_yml);
     $view_id = $values['id'];
